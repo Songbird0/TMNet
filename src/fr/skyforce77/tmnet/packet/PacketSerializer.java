ba61f4956cacd0ab55b4edbc29ed64ba24bce8ff
Songbird0 <halo.spark@laposte.net>
2016-11-09 15:09:03 +0100

    [PacketSerializer] adding javadoc

--- src/fr/skyforce77/tmnet/packet/PacketSerializer.java
+++ src/fr/skyforce77/tmnet/packet/PacketSerializer.java
@@ -1,12 +1,24 @@
 package fr.skyforce77.tmnet.packet;
-
+/**
+* <h1> PacketSerializer class <h1>
+* ...
+* @author Skyforce77
+* @since 2016-11-05
+*/
 public class PacketSerializer {
-	
+	/**
+	* @param packet - description
+	* @return PacketData instance
+	*/
 	public PacketData serialize(Packet packet) {
 		// TODO
 		return null;
 	}
 	
+	/**
+	* @param data - description
+	* @return Packet instance
+	*/
 	public Packet unserialize(PacketData data) {
 		// TODO
 		return null;

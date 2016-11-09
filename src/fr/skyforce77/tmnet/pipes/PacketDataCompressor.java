40536098f816fbb7e4ccf4fe35e5b926f20f8b7e
Songbird0 <halo.spark@laposte.net>
2016-11-09 14:40:48 +0100

    adding javadoc

--- src/fr/skyforce77/tmnet/pipes/PacketDataCompressor.java
+++ src/fr/skyforce77/tmnet/pipes/PacketDataCompressor.java
@@ -2,14 +2,26 @@ package fr.skyforce77.tmnet.pipes;
 
 import fr.skyforce77.tmnet.packet.PacketData;
 
+/**
+* <h1> PacketDataCompressor class <h1>
+* @author Skyforce77
+* @since 2016-11-09
+* @see PacketDataPipe
+*/
 public class PacketDataCompressor implements PacketDataPipe {
 
 	@Override
+	/**
+	* @param data - description
+	*/
 	public void in(PacketData data) {
 		// TODO
 	}
 
 	@Override
+	/**
+	* @param data - description
+	*/
 	public void out(PacketData data) {
 		// TODO
 	}

60fc6d9cca1f633681f54234d4e12197378c17d6
Songbird0 <halo.spark@laposte.net>
2016-11-09 14:22:23 +0100

    adding javadoc

--- src/fr/skyforce77/tmnet/pipes/PacketDataPipe.java
+++ src/fr/skyforce77/tmnet/pipes/PacketDataPipe.java
@@ -2,9 +2,22 @@ package fr.skyforce77.tmnet.pipes;
 
 import fr.skyforce77.tmnet.packet.PacketData;
 
-public interface PacketDataPipe {
+/**
+* <h1> PacketDataPipe interface <h1>
+* ...
+* @author Skyforce77
+* @since 2016-11-05
+*/
+public interface PacketDataPipe 
+{
 	
+	/**
+	* @param data - ...
+	*/
 	public void in(PacketData data);
+	/**
+	* @param data - ...
+	*/
 	public void out(PacketData data);
 
 }

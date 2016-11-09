8110140f11553f54f0ef213e2dd04291a83f7768
Songbird0 <halo.spark@laposte.net>
2016-11-09 15:16:57 +0100

    [PacketData] adding javadoc

--- src/fr/skyforce77/tmnet/packet/PacketData.java
+++ src/fr/skyforce77/tmnet/packet/PacketData.java
@@ -1,23 +1,51 @@
 package fr.skyforce77.tmnet.packet;
 
+/**
+* <h1> PacketData class <h1>
+* ...
+* @author Skyforce77
+* @since 2016-11-05
+*/
 public class PacketData {
 	
+	/**
+	* clazz attribute.
+	*/
 	private String clazz;
+	/**
+	* bytes attribute.
+	*/
 	private byte[] bytes;
 	
+	/**
+	* PacketData constructor
+	* @param clazz - description
+	* @param bytes - description
+	*/
 	public PacketData(String clazz, byte[] bytes) {
 		this.clazz = clazz;
 		this.bytes = bytes;
 	}
 
+	/**
+	* @return String instance
+	*/
 	public String getOriginalClass() {
 		return clazz;
 	}
 
+	/**
+	* Get bytes attribute.
+	* @return A bytes array
+	*/
 	public byte[] getBytes() {
 		return bytes;
 	}
 
+	/**
+	* Set bytes attribute.
+	* @param bytes - A bytes array.
+	*/
 	public void setBytes(byte[] bytes) {
 		this.bytes = bytes;
 	}
